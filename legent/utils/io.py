81a24c116432b316775f4994e1f07011e5e0313a
cheng <chengzl18@163.com>
2024-04-02 04:02:34 +0800

    Update logger name

--- legent/utils/io.py
+++ legent/utils/io.py
@@ -14,7 +14,7 @@ def log(*args):
         format="%(asctime)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
-    logger = logging.getLogger("LIGENT")
+    logger = logging.getLogger("LEGENT")
     logger.error(*args)
 
 
