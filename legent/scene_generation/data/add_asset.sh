26524a02eceba754151a9ea7b7c869e43123dfe0
tommywang <wzthaha@gmail.com>
2024-05-10 01:56:48 +0800

    add asset script

---
+++ legent/scene_generation/data/add_asset.sh
@@ -0,0 +1,5 @@
+python add_objects.py \
+    --asset_type test_type \
+    --asset add_asset_example.json \ 
+    
+    
---
+++ legent/scene_generation/data/add_asset_type.sh
@@ -0,0 +1,10 @@
+python add_objects.py \
+    --asset_type test_type \
+    --inKitchens 1 \
+    --inLivingRooms 1 \ 
+    --inBedrooms 1 \
+    --inBathrooms 1 \
+    --onFloor 1 \
+    --multiplePerRoom 1 \
+    
+    
