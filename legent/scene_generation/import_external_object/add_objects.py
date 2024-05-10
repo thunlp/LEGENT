c7a189eebab0c0098f7b64dbbd28604399b8a35b
tommywang <wzthaha@gmail.com>
2024-05-10 17:37:19 +0800

    modify default env path

--- legent/scene_generation/import_external_object/add_objects.py
+++ legent/scene_generation/import_external_object/add_objects.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 from legent.environment.env_utils import get_default_env_data_path
 
-ENV_DATA_PATH = f"{get_default_env_data_path()}/procthor"
+ENV_DATA_PATH = Path(f"{get_default_env_data_path()}/procthor")
 # ENV_DATA_PATH = Path(r"D:\code\LEGENT\LEGENT\legent\scene_generation\data")
 
 parser = argparse.ArgumentParser()
