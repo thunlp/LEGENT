58dc45f3a1974c5ef770f9ba5683ccfe2507d1ee
cheng <chengzl18@163.com>
2024-10-13 17:02:55 +0800

    Add example for hiding a pointed nearby object

--- scripts/set_object.py
+++ scripts/set_object.py
@@ -15,12 +15,14 @@ After running the script, you can type the following commands in the chat box to
 - hide: Hide the object that the player is looking at.
 - hide <object_id>: Hide the object with the specified object_id.
 - show <object_id>: Show the object with the specified object_id.
+- grab: Hide the object that the player is looking at if the player is close to the object.
 - move: Move the object that the player is looking at to a higher position.
 
 """
 
 from legent import Environment, Observation, Action
 from legent.action.api import HideObject, ShowObject, MoveObject, AgentTargetObjectID, PlayerTargetObjectID
+from legent.utils.math import vec_xz, distance
 import re
 
 env = Environment(env_path="auto")
@@ -40,6 +42,11 @@ try:
         elif obs.text.startswith("show"):
             object_id = int(re.findall(r"\d+", obs.text)[0])
             api_calls.append(ShowObject(object_id))
+        elif obs.text.startswith("grab"):
+            obs = env.step(Action(api_calls=[PlayerTargetObjectID()]))
+            object_id = int(obs.api_returns["object_id"])
+            if distance(vec_xz(obs.game_states["instances"][object_id]["position"]), vec_xz(obs.game_states["player"]["position"])) < 2:
+                api_calls.append(HideObject(object_id))
         elif obs.text == "move":
             obs = env.step(Action(api_calls=[PlayerTargetObjectID()]))
             object_id = int(obs.api_returns["object_id"])
